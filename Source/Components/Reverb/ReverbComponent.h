519dd877bbbcf97a8802250e967da4f5736bfa99
Fernando Garcia de la Cruz <ferchoget@gmail.com>
2023-08-31 21:54:47 -0600

    Started GUI RB implementation

--- Source/Components/Reverb/ReverbComponent.h
+++ Source/Components/Reverb/ReverbComponent.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "../../PluginProcessor.h"
 
 //==============================================================================
 /*
@@ -18,12 +19,34 @@
 class ReverbComponent  : public juce::Component
 {
 public:
-    ReverbComponent();
+    ReverbComponent(AmaranthAudioProcessor&);
     ~ReverbComponent() override;
 
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void addSlider();
 
 private:
+    AmaranthAudioProcessor& processor;
+    
+    juce::Slider roomSizeSlider;
+    juce::Slider dampingSlider;
+    juce::Slider mixSlider;
+    juce::Slider widthSlider;
+    juce::Slider feedbackSlider;
+    
+    juce::Label roomSizeLabel;
+    juce::Label dampingLabel;
+    juce::Label mixLabel;
+    juce::Label widthLabel;
+    juce::Label feedbackLabel;
+    
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> roomSizeAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> dampingAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mixAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> widthAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> feedbackAttach;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ReverbComponent)
 };
