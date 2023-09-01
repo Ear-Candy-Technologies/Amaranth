ba9f54e06a3356d41f13d896f1f22c78f08ba60c
Fernando Garcia de la Cruz <ferchoget@gmail.com>
2023-08-31 19:28:09 -0600

    reverb initial class structure

---
+++ Source/DSP/FX/Reverb/Reverb.h
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    Reverb.h
+    Created: 31 Aug 2023 7:20:52pm
+    Author:  Fernando Garcia
+
+  ==============================================================================
+*/
+#include <JuceHeader.h>
+#pragma once
+class Reverb
+{
+public:
+    Reverb();
+    ~Reverb();
+private:
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Reverb)
+};
